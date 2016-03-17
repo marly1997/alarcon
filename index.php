0f7952ea9679aef971c89de7a4a420766c85f277
Delicia Cierto <deliciacierto@gmail.com>
2016-03-17 09:24:20 -0500

    inicando proyecto

---
+++ index.php
@@ -0,0 +1,39 @@
+<html>
+<head>
+	<<meta charset="utf-8">
+	<<meta http-equiv="X-UA-COMPATIBLE" content="IE=edge">
+	<link >></title>
+	<link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+<div class="contenedor">
+<nav>
+	<ul class= "menu-lista">
+			<li class="menu-item">
+			<a href="" class="menu-link">INICIO</a>
+			</li>
+			<li class="menu-item">
+			<a href=""class="menu-link">GALERIA</a>
+			</li>
+			<li class="menu-item">
+			<a href="" class="menu-link">BLOG</a>
+			</li>
+	<li class="menu-item">
+	<a href="" class="menu-link">ANUNCIOS</a>
+	</li>
+	<li class="menu-item">
+	<a href="" class="menu-link">CONTACTENOS</a>
+	
+	</li>
+	</nav>	
+	<div class="slider">
+	<img src="4.JPG" alt="1000"  width="900">
+	<div class="slider">
+	<h1>Nunca te quejes del ambiente o de los que te rodean. Hay quienes en tu mismo ambiente supieron vencer. Las circunstancias son buenas o malas según la voluntad o fortaleza de tu corazón</h1>
+	<div class="slider">
+	<img src="1.jpg" height="130" nidth"150">
+	<img src="2.JPG" height="130" nidth"150">
+	<img src="3.JPG" height="130" nidth"150">
+	<img src="5.jpg" height="130" nidth"150">
+</body>
+</html>
